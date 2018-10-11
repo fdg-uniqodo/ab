90fc9c7e5ff770e54d302f6d8f01ddbfc68671ba
Frans de Graaff <frans@uniqodo.com>
2018-10-11 09:08:10 +0100

    Add concept of a person which can be a guest or player

--- Player.php
+++ Player.php
@@ -5,10 +5,14 @@ class Player {
     /** @var string */
     public $card;
 
+    public function __construct(Person $person)
+    {
+    }
+
     /**
      * @param string $card
      */
-    public function acceptCard($card) {
+    public function accept($card) {
         $this->card = $card;
     }
 }
